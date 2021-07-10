0383e0c211a0d8c03bd8f1a1368ae43ffd4b991a
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-10 15:31:03 -0600

    Implement methods and views to add friend functionality

---
+++ app/controllers/friends_controller.rb
@@ -0,0 +1,21 @@
+class FriendsController < ApplicationController
+  def create
+    @user = current_user
+    friend = User.find_by(email: params[:email])
+
+    if friend.present?
+      if @user == friend
+        flash[:notice] = 'You have entered your own email!'
+      elsif @user.friends.include?(friend)
+        flash[:notice] = 'You entered a friend who already exists on your list!'
+      else !@user.friends.include?(friend)
+        # @user.friendships.create!(friend: friend)
+        @user.friends.push(friend)
+      end
+    else
+      flash[:error] = "Invalid Email!"
+    end
+
+    redirect_to '/dashboard'
+  end
+end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  def new
+  end
+  
   def create
     found_user = User.find_by(email: params[:email])
     if found_user && found_user.authenticate(params[:password])
@@ -6,7 +9,7 @@ class SessionsController < ApplicationController
       redirect_to "/dashboard"
       # flash[:success] = "Welcome, #{found_user.email}!"
     else 
-      flash[:error] = "Your email or password are incorrect!"
+      flash[:error] = "Either the email or password you have entered is incorrect!"
       redirect_to "/login"
     end 
   end 
