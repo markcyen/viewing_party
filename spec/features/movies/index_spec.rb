5a328186bf8de19125ed716b3aa04d9fd1b34c09
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-12 17:47:16 -0600

    Create test for searching movies

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -11,20 +11,43 @@ RSpec.describe "Movie Index" do
     visit '/discover'
   end
 
-  it "displays top rated movies" do
-    response_body = File.read("spec/fixtures/top_movies.json")
-    stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=fc8f9a61e60adef6d5af436d96592863").
-      to_return(status: 200, body: response_body, headers: {})
-    click_button "Find Top Rated Movies"
+  describe 'top movies' do
+    it "displays top rated movies" do
+      response_body = File.read("spec/fixtures/top_movies_1.json")
+      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['api_key']}").
+        to_return(status: 200, body: response_body, headers: {})
+      click_button "Find Top Rated Movies"
+  
+      expect(current_path).to eq(movies_path)
+      expect(page.status_code).to eq(200)
+      movie_list = find("#top_movies").all("div") 
+      expect(movie_list.size).to eq(20)
+  
+      within("#top_movies") do
+        expect(page).to have_content("The Shawshank Redemption")
+        expect(page).to have_content("Gabriel's Inferno Part II")
+      end 
+    end
+  end
 
-    expect(current_path).to eq(movies_path)
-    expect(page.status_code).to eq(200)
-    movie_list = find("#top_movies").all("div") 
-    expect(movie_list.size).to eq(20)
+  describe 'search movies' do
+    it 'can search for movie titles by keyword' do
+      response_body_search = File.read("spec/fixtures/search_movies.json")
+      stub_request(:get, "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['api_key']}").
+        to_return(status: 200, body: response_body_search, headers: {})
 
-    within("#top_movies") do
-      expect(page).to have_content("The Shawshank Redemption")
-      expect(page).to have_content("Gabriel's Inferno Part II")
-    end 
+      response_body_top = File.read("spec/fixtures/top_movies_1.json")
+      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['api_key']}").
+        to_return(status: 200, body: response_body_top, headers: {})
+
+      click_button "Find Top Rated Movies"
+
+      fill_in 'title', with: 'godfather'
+      click_button 'Find Movies'
+
+      expect(page).to have_content('The Godfather')
+      expect(page).to have_content('The Godfather: Part II')
+      expect(page).to_not have_content("Schindler's List")
+    end
   end
 end
