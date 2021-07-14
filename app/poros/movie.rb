8aab1f2ab5cd54bedba2ad8a29b464964a5c1500
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-07-14 00:09:40 -0600

    Implement movie show page via api consumption

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,9 +1,32 @@
 class Movie
-  attr_reader :title, :vote_average #, :id
+  attr_reader :id, :title, :vote_average, :summary, :duration, :genres
 
   def initialize(movie_data)
-    # @id = movie_data[:id]
+    @id = movie_data[:id]
     @title = movie_data[:title]
     @vote_average = movie_data[:vote_average]
+    @summary = movie_data[:overview]
+    @duration = movie_data[:runtime]
+    @genres = movie_data[:genres]
+  end
+
+  def list_genres
+    @genres.map do |genre|
+      genre[:name]
+    end
+  end
+
+  def list_of_10_cast_members(movie_id)
+    casting = MovieService.find_cast(movie_id)[:cast] 
+    casting.map do |cast|
+      "#{cast[:name]} as #{cast[:character]}"
+    end[0..9]
+  end
+
+  def list_reviews(movie_id)
+    reviews = MovieService.find_reviews(movie_id)
+    reviews.map do |review|
+      "Author: #{review[:author]} \n\n #{review[:content]}"
+    end
   end
 end
